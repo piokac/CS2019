19798531e2a776f1d525308ed75ca025ef26a4f3
Piotr Kaczmarek <piokac@gmail.com>
2019-10-03 15:31:10 +0200

    formatting l1

--- l1/test_1/main.cpp
+++ l1/test_1/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+int main() {
+  cout << "Hello World!" << endl;
+
+  return 0;
 }
